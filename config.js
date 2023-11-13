a59d611dc1fd8e839f88f6d44d1c233846e05cf0
zeki <nguyenhatrung411@gmail.com>
2023-11-13 05:09:07 +0000

    up

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    introTitle: 'Hoàng Thị Vân Anh ơi 🥰',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    btnIntro: 'Anh xin lỗi bé yêu ơi. Mong bé tha lỗi cho anh ạ ',
+    title: '🥺',
+    desc: 'Anh nhớ em bé lắm ạ, bé iêu thấy đáng yêu thì mong bé tha lỗi cho anh nha',
+    btnYes: 'Tạm chấp nhận -_- Còn lần sau thì liệu hồn',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    question: 'Nếu bé iêu đã có lòng thì anh cũng xin có dạ ạ, bé nhắn tin cho anh nha <3',
+    btnReply: 'Gửi cho Trung ngu ngok <3 (bé nhớ bỏ block fb để gửi được nhá)',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    mess: 'Anh biết mà 🥰. Yêu bé nhiều nhiều 😘😘',
+    messDesc: 'Anh hứa sẽ không làm bé buồn nữa đâu ạ.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://www.facebook.com/messages/t/100002958349594' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
